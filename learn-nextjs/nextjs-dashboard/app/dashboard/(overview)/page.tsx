a06c700bddee13c9c36f242eed96eea4c0ffbd53
bmathias12 <bmathias@speedeondata.com>
2024-05-22 12:01:22 -0400

    push data fetching down to components

--- learn-nextjs/nextjs-dashboard/app/dashboard/(overview)/page.tsx
+++ learn-nextjs/nextjs-dashboard/app/dashboard/(overview)/page.tsx
@@ -2,11 +2,6 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { 
-  //fetchRevenue, 
-  //fetchLatestInvoices,
-  fetchCardData
-} from '@/app/lib/data';
 import { Suspense } from 'react';
 import { 
   RevenueChartSkeleton, 
@@ -16,9 +11,6 @@ import {
 
  
 export default async function Page() {
-  //const revenue = await fetchRevenue();
-  //const latestInvoices = await fetchLatestInvoices();
-  const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
 
   return (
     <main>
