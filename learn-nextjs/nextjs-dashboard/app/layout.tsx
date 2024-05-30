be9c3c764f340183b188d2a8076aa6548e754511
bmathias12 <bmathias@speedeondata.com>
2024-05-30 13:58:34 -0400

    add metadata to layout page

--- learn-nextjs/nextjs-dashboard/app/layout.tsx
+++ learn-nextjs/nextjs-dashboard/app/layout.tsx
@@ -1,6 +1,12 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
  
+export const metadata: Metadata = {
+  title: 'Acme Dashboard',
+  description: 'The official Next.js Course Dashboard, built with App Router.',
+};
+
 export default function RootLayout({
   children,
 }: {
