033ef5ae9ff75cfc425185534252a9c3f5f8d6be
bmathias12 <bmathias@speedeondata.com>
2024-05-30 10:59:06 -0400

    add new invoices to db

---
+++ learn-nextjs/nextjs-dashboard/app/lib/actions.ts
@@ -0,0 +1,59 @@
+'use server';
+
+import pg from 'pg'
+import { z } from 'zod';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+
+require('dotenv').config();
+
+const { Client } = pg
+
+const client = new Client({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: parseInt(process.env.DB_PORT || '5432')
+});
+async function connectToDatabase() {
+  await client.connect();
+}
+
+// Call the function
+connectToDatabase();
+ 
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+ 
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+      customerId: formData.get('customerId'),
+      amount: formData.get('amount'),
+      status: formData.get('status'),
+    });
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    // Insert the new invoice into the database
+    try {
+        await client.query(
+          `INSERT INTO invoices (customer_id, amount, status, date)
+           VALUES ($1, $2, $3, $4)`,
+          [customerId, amountInCents, status, date]
+        );
+    } catch (error) {
+      console.error('Database Error:', error);
+      throw new Error('Failed to enter invoice.');
+    }
+    revalidatePath('/dashboard/invoices');
+    redirect('/dashboard/invoices');
+  }
